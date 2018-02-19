d29e4fc7954677fdf90bf62f0d0868e5b719c221
Stephanie Hardy <stephanie.hardy@wework.com>
2018-02-19 22:44:12 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,31 @@
 # Write your #display_rainbow method here
+
+ red = [R: red] 
+ orange = [O: orange]
+ yellow = [Y: yelolow]
+ green = [G: green]
+
+def #display_rainbow(['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'])
+
+puts
+
+ red = [R: red] 
+ orange = [O: orange]
+ yellow = [Y: yelolow]
+ green = [G: green]
+ blue = [B: blue]
+ indigo = [I: indigo]
+ violet = [V: violet]
+
+end
+
+
+
+
+
+
+
+
+
+  
+  
